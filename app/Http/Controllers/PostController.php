73b74bc8af6dd93b68e2d13e69d72ca6b5ae36ff
dvash999 <myselfandme@gmail.com>
2019-08-16 22:11:27 +0300

    m

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Post::getAllPosts();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        dd('create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        dd('store');
+        return Post::storePost($request->post());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Post::showPost($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        dd($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        dd($request->post());
+        $post = $request->post();
+        return DB::transaction(function ($post ,$id)
+        {
+            Post::deletePost($id);
+            Post::storePost($post);
+        });
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Models/Post.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class Post extends Model
+{
+    public static function getAllPosts()
+    {
+        return self::all();
+    }
+
+    public static function storePost($post)
+    {
+        $isSaved = DB::transaction(function () use ($post) {
+                    $newPost = new Post;
+                    foreach ($post as $key => $value) {
+                        $newPost[$key] = $post[$key];
+                    }
+                    $newPost->save();
+
+                    return true;
+                });
+
+        if ($isSaved) {
+            return response()->json(['message' => 'success', 'status' => 200]);
+        } else {
+            return response()->json(['message' => 'failed', 'status' => 401]);
+        }
+
+    }
+
+    public static function showPost($id)
+    {
+        return self::where('id', $id)->first();
+    }
+
+    public static function deletePost($id)
+    {
+        $postToDelete = self::find($id);
+        return $postToDelete->delete() ? 'deleted successfully' : 'failed';
+    }
+
+
+}
